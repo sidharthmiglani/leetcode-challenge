cf5b7d2903560f059d7aa9e522090bb98423a43d
Sidharth Miglani <sidhumig@gmail.com>
2020-04-13 08:29:14 -0700

    Iterative Day 12

--- lastStoneWeight.js
+++ lastStoneWeight.js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 
+//ITERATIVE METHOD
+var lastStoneWeight = function(stones) {
+    while(stones.length >1){
+        stones.sort((a, b) => a - b);// sorts number array
+        let x = stones.pop();
+        let y = stones.pop();
+        if(x !== y){
+            stones.push(Math.abs(x-y));
+        }
+    }
+    if (stones.length == 1){
+        return stones[0];
+    }
+    else if(stones.length == 0){
+        return 0;
+    }
+};
+
+ // RECURSIVE METHOD
 var lastStoneWeight = function(stones) {
     if (stones.length == 1){
         return stones[0];
